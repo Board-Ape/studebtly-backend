c067510f1c0fcec8a0c2c46a8b9738351eecd710
Sam Singer <samuel.singer1@gmail.com>
2018-02-27 12:32:00 -0700

    GET request displaying for all schools

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ app.get('/', function(request, response) {
   );
 });
 
+// GET REQUEST ALL COLLEGES
+app.get('/api/v1/colleges', (request, response) => {
+  database('colleges').select()
+    .then(college => response.status(200).json(college))
+    .catch(error => response.status(500).json({ error }));
+});
+
 app.listen(app.get('port'), () => {
   //eslint-disable-next-line
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
