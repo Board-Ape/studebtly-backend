7d2eea571314070e01eb9aa467d167daea41858a
Sam Singer <samuel.singer1@gmail.com>
2018-02-27 16:20:43 -0700

    College action fetch, colleges container and colleges card

--- studebtly-frontend/src/actions/index.js
+++ studebtly-frontend/src/actions/index.js
@@ -2,3 +2,11 @@ export const getColleges = colleges => ({
   type: 'GET_COLLEGES',
   colleges
 });
+
+export const fetchColleges = () => dispatch => {
+  fetch('api/v1/colleges')
+    .then(response => response.json())
+    .then(parsedResponse => dispatch(getColleges(parsedResponse)))
+    //eslint-disable-next-line
+    .catch(error => console.log(`Error has occurred: ${error}`));
+};
