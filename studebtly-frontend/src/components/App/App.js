dc2717feb54bbdd6bb507d7a88f60f93f6634afd
Sam Singer <samuel.singer1@gmail.com>
2018-02-27 11:21:32 -0700

    App file routes

--- studebtly-frontend/src/components/App/App.js
+++ studebtly-frontend/src/components/App/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import NavBar from '../../components/NavBar/NavBar';
+import Landing from '../../components/Landing/Landing';
+import SchoolCardContainer from '../../containers/SchoolCardContainer/SchoolCardContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World!
+        <Route path="/" component={NavBar} />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/colleges" component={SchoolCardContainer} />
       </div>
     );
   }
