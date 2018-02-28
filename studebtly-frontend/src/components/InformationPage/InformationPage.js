459bd03d5ffafe1358f88ff25e206b98ee4f0b40
Sam Singer <samuel.singer1@gmail.com>
2018-02-27 20:13:11 -0700

    InformationPage is now present

---
+++ studebtly-frontend/src/components/InformationPage/InformationPage.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './InformationPage.css';
+import CollegesPage from '../BottlesPage/BottlesPage.js';
+import HomePage from '../HomePage/HomePage.js';
+
+export default class InformationPage extends Component {
+  constructor() {
+    super();
+    this.state ={
+      CB: null,
+      HB: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      CB: this.props.CB,
+      HB: this.props.HB
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      CB: nextProps.CB,
+      HB: nextProps.HB
+    });
+  }
+
+  renderPage = () => {
+    if (this.state.CB === true) {
+      return <CollegesPage />;
+    }
+    if (this.state.HB === true) {
+      return <HomePage />;
+    }
+  };
+
+  render() {
+    return (
+      <section id='informationpage-container'>
+        {
+          this.renderPage()
+        }
+      </section>
+    );
+  }
+}
+
+InformationPage.propTypes = {
+  CB: PropTypes.bool,
+  HB: PropTypes.bool
+};
